97e6b04c98728b0b0c5c0adc73b1714f434e1c7d
Lorenzo Collodi <lore.1997@live.it>
2018-10-17 10:44:23 +0200

    First commit

--- Swift_001/ViewController.swift
+++ Swift_001/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBOutlet weak var heyLabel: UILabel!
+    @IBAction func coolButton(_ sender: Any) {
+        heyLabel.text="Buttons are cool!"
+    }
+    
+    var buttonTapped=0
+    
+    @IBAction func buttonMain(_ sender: UIButton) {
+        buttonTapped+=1
+        if buttonTapped<10 {heyLabel.text="Hey man"}
+        else{
+            heyLabel.text="you overtapped"
+        }
+         
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        heyLabel.text="Hello man"
+    
     }
 
 
